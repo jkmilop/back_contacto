0b55bf9735df8bf10183ed65a54d3e963f076937
DESKTOP-KV9U18O\Cwng <jkmilop@gmail.com>
2022-11-02 18:21:15 -0500

    CORS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import morgan from "morgan";
-
+import cors from "cors";
 const app = express();
 
 import routes from "./routes/contacto.routes.js";
 
 // Middlewares
+
+app.use(cors());
+  
 app.use(morgan("dev"));
 app.use(express.json());
 
