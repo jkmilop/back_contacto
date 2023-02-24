74ec61c16a8de81ae40c1efabbfa2a2bd6fc06bb
Juan Camilo Peña <jkmilop@gmail.com>
2023-02-24 17:31:12 -0500

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -2,8 +2,8 @@ import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize(
   "libreta", // db name,
-  "root", // username
-  "cwng", // password
+  "", // username
+  "", // password
   {
     host: "localhost",
     dialect: "mysql",
